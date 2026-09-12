bfe70867f298505204b8445f76e384f366207f23
kerwin_lv <2509456238@qq.com>
2026-09-12 10:42:03 +0800

    删除不必套代码

--- modules/systemConfig/router.go
+++ modules/systemConfig/router.go
@@ -1,18 +1,11 @@
 /*
  * @Author       : lvyitao
  * @Date         : 2024-06-07 16:41:03
- * @LastEditTime: 2026-09-12 09:51:34
+ * @LastEditTime: 2026-09-12 10:41:16
  */
 package systemConfig
 
 import (
-	// "log"
-
-	// "src/common"
-	// "src/utils"
-
-	// "github.com/go-co-op/gocron/v2"
-	// v1 "src/modules/systemConfig/v1"
 
 	v1 "src/modules/systemConfig/v1"
 
@@ -23,14 +16,5 @@ func Init(factoryRouter fiber.Router) {
 	// 接口
 
 	factoryRouter.Get("/ts", v1.Ts)
-	// factoryRouter.Get("/get/images", v1.GetImg)
 
-	// 定时任务
-	// _, err := common.GoCronTab.NewJob(
-	// 	gocron.CronJob("0 0 9 ? * MON", true),
-	// 	gocron.NewTask(utils.TimeCleanlog),
-	// )
-	// if err != nil {
-	// 	log.Println(err)
-	// }
 }
